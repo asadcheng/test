95d8b4489462f80abb56309c529be0a74256a293
asadcheng <asadcheng@shaguatech.com>
2014-11-30 15:20:12 -0800

    This is the another person test

--- src/main.c
+++ src/main.c
@@ -5,7 +5,10 @@ int main(void)
 
      printf("This is for #iss53 be resolved\n");
      printf("Now this is for the conflic between develop branch and iss53 branch test");
-     printf("This is the second changed of #iss53\n");
+     printf("This is the second changed of #iss53\n"); 
 
+
+     printf("This is the other person test\n");
+ 
      return 0;
 }
